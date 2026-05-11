2508b62bad06bb236136b653cae3ff7af5ff6796
Michael <12264379+akhotam@users.noreply.github.com>
2026-05-11 11:32:55 -0700

    working bar chart

--- Homework2/mmakhota/main.js
+++ Homework2/mmakhota/main.js
@@ -1,177 +1,100 @@
-let abFilter = 25;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-let scatterLeft = 0, scatterTop = 0;
-let scatterMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    scatterWidth = 400 - scatterMargin.left - scatterMargin.right,
-    scatterHeight = 350 - scatterMargin.top - scatterMargin.bottom;
+const barMargin = { top: 60, right: 40, bottom: 60, left: 60 };
+const barWidth  = Math.round(width * 0.45) - barMargin.left - barMargin.right;
+const barHeight = Math.round(height * 0.5) - barMargin.top - barMargin.bottom;
 
-let distrLeft = 400, distrTop = 0;
-let distrMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    distrWidth = 400 - distrMargin.left - distrMargin.right,
-    distrHeight = 350 - distrMargin.top - distrMargin.bottom;
-
-let teamLeft = 0, teamTop = 400;
-let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    teamWidth = width - teamMargin.left - teamMargin.right,
-    teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
-
-// plots
-d3.csv("players.csv").then(rawData =>{
-    console.log("rawData", rawData);
-
-    rawData.forEach(function(d){
-        d.AB = Number(d.AB);
-        d.H = Number(d.H);
-        d.salary = Number(d.salary);
-        d.SO = Number(d.SO);
-    });
-
-
-    const filteredData = rawData.filter(d=>d.AB>abFilter);
-    const processedData = filteredData.map(d=>{
-                          return {
-                              "H_AB":d.H/d.AB,
-                              "SO_AB":d.SO/d.AB,
-                              "teamID":d.teamID,
-                          };
-    });
-    console.log("processedData", processedData);
-
-    //plot 1: Scatter Plot
+// load data
+d3.csv("pokemon.csv").then(rawData => {
     const svg = d3.select("svg");
 
-    const g1 = svg.append("g")
-                .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
-                .attr("height", scatterHeight + scatterMargin.top + scatterMargin.bottom)
-                .attr("transform", `translate(${scatterMargin.left}, ${scatterMargin.top})`);
+    // count generations
+    const genCounts = d3.nest()
+        .key(d => d.Generation)
+        .rollup(v => v.length)
+        .entries(rawData)
+        .sort((a, b) => +a.key - +b.key);
 
-    // X label
-    g1.append("text")
-    .attr("x", scatterWidth / 2)
-    .attr("y", scatterHeight + 50)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("H/AB");
+    // colorization
+    const colors = d3.schemeTableau10;
 
+    // chart
+    const gBar = svg.append("g")
+        .attr(
+            "transform",
+            `translate(${barMargin.left}, ${barMargin.top})`
+        );
 
-    // Y label
-    g1.append("text")
-    .attr("x", -(scatterHeight / 2))
-    .attr("y", -40)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("SO/AB");
+    // title
+    gBar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", -20)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .attr("font-weight", "bold")
+        .text("Pokemon Count by Generation");
 
-    // X ticks
-    const x1 = d3.scaleLinear()
-    .domain([0, d3.max(processedData, d => d.H_AB)])
-    .range([0, scatterWidth]);
+    // x scale
+    const x = d3.scaleBand()
+        .domain(genCounts.map(d => d.key))
+        .range([0, barWidth])
+        .padding(0.2);
 
-    const xAxisCall = d3.axisBottom(x1)
-                        .ticks(7);
-    g1.append("g")
-    .attr("transform", `translate(0, ${scatterHeight})`)
-    .call(xAxisCall)
-    .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
+    // y scale
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(genCounts, d => d.value)])
+        .nice()
+        .range([barHeight, 0]);
 
-    // Y ticks
-    const y1 = d3.scaleLinear()
-    .domain([0, d3.max(processedData, d => d.SO_AB)])
-    .range([scatterHeight, 0]);
+    // x axis
+    gBar.append("g")
+        .attr("transform", `translate(0, ${barHeight})`)
+        .call(d3.axisBottom(x));
 
-    const yAxisCall = d3.axisLeft(y1)
-                        .ticks(13);
-    g1.append("g").call(yAxisCall);
+    // y axis
+    gBar.append("g")
+        .call(d3.axisLeft(y));
 
-    // circles
-    const circles = g1.selectAll("circle").data(processedData);
+    // x label
+    gBar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", barHeight + 45)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "13px")
+        .text("Generation");
 
-    circles.enter().append("circle")
-         .attr("cx", d => x1(d.H_AB))
-         .attr("cy", d => y1(d.SO_AB))
-         .attr("r", 5)
-         .attr("fill", "#69b3a2");
-
-    const g2 = svg.append("g")
-                .attr("width", distrWidth + distrMargin.left + distrMargin.right)
-                .attr("height", distrHeight + distrMargin.top + distrMargin.bottom)
-                .attr("transform", `translate(${distrLeft}, ${distrTop})`);
-
-    //plot 2: Bar Chart for Team Player Count
-
-    const teamCounts = processedData.reduce((s, { teamID }) => (s[teamID] = (s[teamID] || 0) + 1, s), {});
-    const teamData = Object.keys(teamCounts).map((key) => ({ teamID: key, count: teamCounts[key] }));
-    console.log("teamData", teamData);
-
-
-    const g3 = svg.append("g")
-                .attr("width", teamWidth + teamMargin.left + teamMargin.right)
-                .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
-                .attr("transform", `translate(${teamMargin.left}, ${teamTop})`);
-
-    // X label
-    g3.append("text")
-    .attr("x", teamWidth / 2)
-    .attr("y", teamHeight + 50)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("Team");
-
-
-    // Y label
-    g3.append("text")
-    .attr("x", -(teamHeight / 2))
-    .attr("y", -40)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("Number of players");
-
-    // X ticks
-    const x2 = d3.scaleBand()
-    .domain(teamData.map(d => d.teamID))
-    .range([0, teamWidth])
-    .paddingInner(0.3)
-    .paddingOuter(0.2);
-
-    const xAxisCall2 = d3.axisBottom(x2);
-    g3.append("g")
-    .attr("transform", `translate(0, ${teamHeight})`)
-    .call(xAxisCall2)
-    .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
-
-    // Y ticks
-    const y2 = d3.scaleLinear()
-    .domain([0, d3.max(teamData, d => d.count)])
-    .range([teamHeight, 0])
-    .nice();
-
-    const yAxisCall2 = d3.axisLeft(y2)
-                        .ticks(6);
-    g3.append("g").call(yAxisCall2);
+    // y label
+    gBar.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -barHeight / 2)
+        .attr("y", -45)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "13px")
+        .text("Number of Pokemon");
 
     // bars
-    const bars = g3.selectAll("rect").data(teamData);
+    gBar.selectAll(".bar")
+        .data(genCounts)
+        .enter()
+        .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(d.key))
+            .attr("y", d => y(d.value))
+            .attr("width", x.bandwidth())
+            .attr("height", d => barHeight - y(d.value))
+            .attr("fill", (d, i) => colors[i % colors.length]);
 
-    bars.enter().append("rect")
-    .attr("y", d => y2(d.count))
-    .attr("x", d => x2(d.teamID))
-    .attr("width", x2.bandwidth())
-    .attr("height", d => teamHeight - y2(d.count))
-    .attr("fill", "steelblue");
-
-
-    }).catch(function(error){
-    console.log(error);
+    // bar labels
+    gBar.selectAll(".bar-label")
+        .data(genCounts)
+        .enter()
+        .append("text")
+            .attr("class", "bar-label")
+            .attr("x", d => x(d.key) + x.bandwidth() / 2)
+            .attr("y", d => y(d.value) - 6)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "11px")
+            .attr("font-weight", "bold")
+            .text(d => d.value);
 });
