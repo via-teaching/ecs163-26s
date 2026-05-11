71e71d26b76e5285a304a952615650999bf52962
Adrian Perez <appperez@ucdavis.edu>
2026-05-10 19:52:12 -0700

    Add stacked bar chart

--- Homework2/appperez/main.js
+++ Homework2/appperez/main.js
@@ -4,15 +4,17 @@ const height = window.innerHeight;
 
 let sankeyLeft = 0,
   sankeyTop = 0;
-let sankeyMargin = { top: 15, right: 50, bottom: 15, left: 20 },
+let sankeyMargin = { top: 50, right: 50, bottom: 15, left: 20 },
   sankeyWidth = width - sankeyMargin.left * 2 - sankeyMargin.right * 2,
   sankeyHeight = height / 2 - sankeyMargin.top - sankeyMargin.bottom;
 
-let distrLeft = 400,
-  distrTop = 0;
-let distrMargin = { top: 10, right: 30, bottom: 30, left: 60 },
-  distrWidth = 400 - distrMargin.left - distrMargin.right,
-  distrHeight = 350 - distrMargin.top - distrMargin.bottom;
+let titleBarHeight = 35;
+
+let stackedBarLeft = 400,
+  stackedBarTop = 0;
+let stackedBarMargin = { top: (height/2 + titleBarHeight + sankeyMargin.bottom + 10), right: 70, bottom: 10, left: 20 },
+  stackedBarWidth = (width/2)
+  stackedBarHeight = (height / 2) 
 
 let teamLeft = 0,
   teamTop = 400;
@@ -20,7 +22,6 @@ let teamMargin = { top: 10, right: 30, bottom: 30, left: 60 },
   teamWidth = width - teamMargin.left - teamMargin.right,
   teamHeight = height - 450 - teamMargin.top - teamMargin.bottom;
 
-let titleBarHeight = 35;
 
 // plots
 d3.csv("fitness_data.csv").then((rawData) => {
@@ -43,7 +44,7 @@ d3.csv("fitness_data.csv").then((rawData) => {
     .attr("text-anchor", "middle")
     .attr("font-size", 24)
     .attr("font-weight", 900)
-    .text("How has fitness wearables impacted these 30 people?");
+    .text("How has fitness wearables influenced these 30 people?");
 
   const g1 = svg
     .append("g")
@@ -59,8 +60,8 @@ d3.csv("fitness_data.csv").then((rawData) => {
     processed_datum = {};
 
     // processed_datum["Wearable use frequency"] = d["How frequently do you use your fitness wearable?"]
-    processed_datum["Did wearable motivate person to exercise?"] =
-      "Influence decision to exercise: " +
+    processed_datum["Did wearable influenced person to exercise?"] =
+      "Influence more exercise: " +
       d[
         "Has using a fitness wearable influenced your decision? [To exercise more?]"
       ];
@@ -73,12 +74,12 @@ d3.csv("fitness_data.csv").then((rawData) => {
       "Exercises " + d["How often do you exercise in a week?"].toLowerCase();
 
     return processed_datum;
-  });
+  })
   filteredData["columns"] = [
-    "Did wearable motivate person to exercise?",
+    "Did wearable influenced person to exercise?",
     "Was the exercise more enjoyable?",
     "Exercise weekly frequency.",
-  ];
+  ]
 
   // Docs used for learning to how to create the diagram: https://observablehq.com/@d3/parallel-sets
   graph = (() => {
@@ -243,130 +244,124 @@ d3.csv("fitness_data.csv").then((rawData) => {
     .attr("fill-opacity", 0.7)
     .text((d) => ` (${d.value.toLocaleString()})`);
 
-  // X label
-  // g1.append("text")
-  // .attr("x", scatterWidth / 2)
-  // .attr("y", scatterHeight + scatterMargin.bottom + scatterMargin.top)
-  // .attr("font-size", "20px")
-  // .attr("text-anchor", "middle")
-  // .text("H/AB");
+  // Stacked bar chart
+  // Help from: https://observablehq.com/@d3/stacked-bar-chart/2
+  const stackedBarData = (() => {
+    processed_datum = {};
+    responsesCounts = new d3.InternMap([], JSON.stringify)
+    processed_data = []
 
-  // // Y label
-  // g1.append("text")
-  // .attr("x", -(scatterHeight / 2))
-  // .attr("y", -40)
-  // .attr("font-size", "20px")
-  // .attr("text-anchor", "middle")
-  // .attr("transform", "rotate(-90)")
-  // .text("SO/AB");
+  // make all possible combinations
+  const influences = new Set();
+  const useFrequencies = new Set();
+  rawData.forEach(d => {
+    const influence = "Influence more exercise: " + (d["Has using a fitness wearable influenced your decision? [To exercise more?]"] || "").trim();
+    const useFrequency = (d["How frequently do you use your fitness wearable?"] || "").trim();
+    influences.add(influence);
+    useFrequencies.add(useFrequency);
+  })
 
-  // // X ticks
-  // const x1 = d3.scaleLinear()
-  // .domain([0, d3.max(processedData, d => d.H_AB)])
-  // .range([0, scatterWidth]);
+  for (const influence of influences) {
+    for (const useFrequency of useFrequencies) {
+      responsesCounts.set([influence, useFrequency], 0)
+    }
+  }
 
-  // const xAxisCall = d3.axisBottom(x1)
-  //                     .ticks(7);
-  // // Adding on the x-axis
-  // g1.append("g")
-  // .attr("transform", `translate(0, ${scatterHeight})`) // puts it in the correct location
-  // .call(xAxisCall)
-  // .selectAll("text")
-  //     .attr("y", "10")
-  //     .attr("x", "-5")
-  //     .attr("text-anchor", "end")
-  //     .attr("transform", "rotate(-40)");
+    rawData.forEach((d) => {
+      influence =
+      "Influence more exercise: " +
+      d[
+        "Has using a fitness wearable influenced your decision? [To exercise more?]"
+      ]
+      useFrequency = d["How frequently do you use your fitness wearable?"]
+      // influence is key[0] and frequency is key[1]
+      key = [influence, useFrequency]
+      responsesCounts.set(key, responsesCounts.get(key) + 1)
+    })
 
-  // // Y ticks
-  // const y1 = d3.scaleLinear()
-  // .domain([0, d3.max(processedData, d => d.SO_AB)])
-  // .range([scatterHeight, 0]);
+    for (const [k, v] of responsesCounts) {
+      processed_data.push({influence: k[0], useFrequency: k[1], count: v})
+    }
 
-  // const yAxisCall = d3.axisLeft(y1)
-  //                     .ticks(13);
+    return processed_data
+  }) ()
 
-  // // this doesn't need to be transformed because it gets put at 0,0 of g1
-  // g1.append("g").call(yAxisCall);
 
-  // // circles
-  // const circles = g1.selectAll("circle").data(processedData);
+  const indexMap = d3.index(stackedBarData, d => d.useFrequency, d => d.influence);
 
-  // circles.enter().append("circle")
-  //      .attr("cx", d => x1(d.H_AB))
-  //      .attr("cy", d => y1(d.SO_AB))
-  //      .attr("r", 5)
-  //      .attr("fill", "#69b3a2");
+  const keys = d3.union(stackedBarData.map(d => d.influence))
+  const series = d3.stack()
+    .keys(keys).order(series => d3.range(series.length).reverse())
+    .value(([, D], key) => D.get(key) && D.get(key).count || 0)
+    (indexMap)
+  
+  const freqOrder = ["Rarely","1-2 times a week","3-4 times a week","Daily"]
+  const x = d3.scaleBand()
+    .domain(freqOrder)
+    .range([stackedBarMargin.left - 10, stackedBarWidth - stackedBarMargin.right])
+    .padding(0.1)
 
-  // const g2 = svg.append("g")
-  //             .attr("width", distrWidth + distrMargin.left + distrMargin.right)
-  //             .attr("height", distrHeight + distrMargin.top + distrMargin.bottom)
-  //             .attr("transform", `translate(${distrLeft}, ${distrTop})`);
+  const y = d3.scaleLinear()
+      .domain([0, d3.max(series, d => d3.max(d, d => d[1])) + 1])
+      .rangeRound([stackedBarHeight * 2 + stackedBarMargin.bottom - 30, stackedBarMargin.top]);
 
-  // //plot 2: Bar Chart for Team Player Count
+  svg.append("g")
+    .selectAll()
+    .data(series)
+    .join("g")
+      .attr("fill", d => color(d.key))
+    .selectAll("rect")
+    .data(D => D.map(d => (d.key = D.key, d)))
+    .join("rect")
+      .attr("x", d => x(d.data[0]))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
+      .attr("width", x.bandwidth())
+    .append("title")
+      .text(d => `Exercise Frequency: ${d.data[0]}\n${d.key}\nCount: ${(d.data[1].get(d.key).count)}`);
+  
+      console.log(stackedBarHeight*2 + stackedBarMargin.bottom)
+  svg.append("g")
+      .attr("transform", `translate(0,${stackedBarHeight*2 + stackedBarMargin.bottom - 30})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0))
 
-  // const teamCounts = processedData.reduce((s, { teamID }) => (s[teamID] = (s[teamID] || 0) + 1, s), {});
-  // const teamData = Object.keys(teamCounts).map((key) => ({ teamID: key, count: teamCounts[key] }));
-  // console.log("teamData", teamData);
+  svg.append("g")
+    .attr("transform", `translate(${stackedBarMargin.left +3}, 0)`)
+    .call(d3.axisLeft(y).ticks(null, "s"))
+  
+  // Title for stack bar plot
+  svg.append("text")
+    .attr("x", stackedBarWidth/2 )
+    .attr("y", height / 2 + titleBarHeight*2)
+    .attr("text-anchor", "middle")
+    .attr("font-size", 16)
+    .attr("font-weight", "bold")
+    .text("How often did the influenced users wear their wearables?");
 
-  // const g3 = svg.append("g")
-  //             .attr("width", teamWidth + teamMargin.left + teamMargin.right)
-  //             .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
-  //             .attr("transform", `translate(${teamMargin.left}, ${teamTop})`);
+  // Add the legend
+  const legendKeys = ["Influence more exercise: Strongly agree ", "Influence more exercise: Agree", "Influence more exercise: Neutral"]
+  const legendX = width / 2 - 330
+  const legendY = 15
+  let size = 20
+  svg.selectAll("mydots")
+  .data(legendKeys)
+  .enter()
+  .append("rect")
+    .attr("y", legendY)
+    .attr("x", function(d,i){ return legendX + i*(size+300)}) 
+    .attr("width", size)
+    .attr("height", size)
+    .style("fill", function(d){ return color(d)})
 
-  // // X label
-  // g3.append("text")
-  // .attr("x", teamWidth / 2)
-  // .attr("y", teamHeight + 50)
-  // .attr("font-size", "20px")
-  // .attr("text-anchor", "middle")
-  // .text("Team");
-
-  // // Y label
-  // g3.append("text")
-  // .attr("x", -(teamHeight / 2))
-  // .attr("y", -40)
-  // .attr("font-size", "20px")
-  // .attr("text-anchor", "middle")
-  // .attr("transform", "rotate(-90)")
-  // .text("Number of players");
-
-  // // X ticks
-  // const x2 = d3.scaleBand()
-  // .domain(teamData.map(d => d.teamID))
-  // .range([0, teamWidth])
-  // .paddingInner(0.3)
-  // .paddingOuter(0.2);
-
-  // const xAxisCall2 = d3.axisBottom(x2);
-  // g3.append("g")
-  // .attr("transform", `translate(0, ${teamHeight})`)
-  // .call(xAxisCall2)
-  // .selectAll("text")
-  //     .attr("y", "10")
-  //     .attr("x", "-5")
-  //     .attr("text-anchor", "end")
-  //     .attr("transform", "rotate(-40)");
-
-  // // Y ticks
-  // const y2 = d3.scaleLinear()
-  // .domain([0, d3.max(teamData, d => d.count)])
-  // .range([teamHeight, 0])
-  // .nice();
-
-  // const yAxisCall2 = d3.axisLeft(y2)
-  //                     .ticks(6);
-  // g3.append("g").call(yAxisCall2);
-
-  // // bars
-  // const bars = g3.selectAll("rect").data(teamData);
-
-  // bars.enter().append("rect")
-  // .attr("y", d => y2(d.count))
-  // .attr("x", d => x2(d.teamID))
-  // .attr("width", x2.bandwidth())
-  // .attr("height", d => teamHeight - y2(d.count))
-  // .attr("fill", "steelblue");
-
-  // }).catch(function(error){
-  // console.log(error);
+  svg.selectAll("mylabels")
+  .data(legendKeys)
+  .enter()
+  .append("text")
+    .attr("y", legendY + size*1.4)
+    .attr("x", function(d,i){ return legendX + i*(size+300) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+    .text(function(d){ return d})
+    .attr("font-size", 10)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .style("alignment-baseline", "middle")
 });
