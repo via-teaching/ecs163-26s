d5f757619602ff4617c120f7543b54b3c65de524
Nithin Senthil <nithin020304@gmail.com>
2026-05-14 12:55:38 -0700

    feat: add initial sankey with formatting on page

--- Homework2/nsen/main.js
+++ Homework2/nsen/main.js
@@ -1,6 +1,8 @@
 let abFilter = 25;
-const width = window.innerWidth / 2;
-const height = window.innerHeight / 2;
+// const width = window.innerWidth;
+// const height = window.innerHeight;
+const width = window.innerWidth / 3;
+const height = window.innerHeight / 3;
 // const width = 400;
 // const height = 350;
 
@@ -9,6 +11,10 @@ let barChartMargin = { top: 40, right: 30, bottom: 85, left: 70 },
   barChartHeight = height - barChartMargin.top - barChartMargin.bottom;
 
 let pieMargin = 30;
+
+let sankeyMargin = { top: 40, right: 30, bottom: 30, left: 80 },
+  sankeyWidth = width - sankeyMargin.left - sankeyMargin.right,
+  sankeyHeight = height - sankeyMargin.top - sankeyMargin.bottom;
 
 // plots
 d3.csv("student_mental_health.csv")
@@ -31,9 +37,13 @@ d3.csv("student_mental_health.csv")
     console.log("processedData", processedData);
 
     // plot 1: Bar chart
-    const barSvg = d3.select("#bar-svg");
+    const barSvg = d3.selectAll("#bar-svg");
 
-    const g1 = barSvg.append("g").attr("width", width).attr("height", height);
+    const g1 = barSvg
+      .append("g")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height);
 
     // Reduce data to category counts
     let barData = d3.rollup(
@@ -130,7 +140,7 @@ d3.csv("student_mental_health.csv")
 
     const radius = Math.min(pieWidth, pieHeight) / 2 - pieMargin * 2;
 
-    const pieSvg = d3.select("#pie-svg");
+    const pieSvg = d3.selectAll("#pie-svg");
 
     // const data_pie1 = { a: 20, b: 80 };
     const pieData = d3.rollup(
@@ -165,49 +175,139 @@ d3.csv("student_mental_health.csv")
       b: 100 - pieData.get("year 4"),
     };
     drawPie(pieSvg, data_pie4, pieWidth, pieHeight, radius, 1, 1, "Year 4");
-
+    
+    // Legend
     const g2 = pieSvg
       .append("g")
       .attr("width", width)
       .attr("height", height)
-      .attr("transform", `translate(${300},${100})`);
+      .attr("transform", `translate(${200},${50})`);
 
-    // Legend
     g2.append("text")
       .attr("font-size", "10px")
       .attr("text-anchor", "left")
       .text("Key");
 
     // Color 1
-    const item1 = g2
-      .append("g")
-      .attr("transform", `translate(${-20},${10})`);
+    const item1 = g2.append("g").attr("transform", `translate(${-20},${10})`);
 
-    item1.append("rect")
+    item1
+      .append("rect")
       .attr("width", "10px")
       .attr("height", "10px")
       .attr("fill", "#98abc5");
-      item1.append("text")
+    item1
+      .append("text")
       .attr("font-size", "10px")
       .attr("text-anchor", "left")
       .attr("transform", `translate(${15},${8})`)
       .text("Depressed");
-    
-    // Color 2
-    const item2 = g2
-      .append("g")
-      .attr("transform", `translate(${-20},${30})`);
 
-    item2.append("rect")
+    // Color 2
+    const item2 = g2.append("g").attr("transform", `translate(${-20},${30})`);
+
+    item2
+      .append("rect")
       .attr("width", "10px")
       .attr("height", "10px")
       .attr("fill", "#8a89a6");
-      item2.append("text")
+    item2
+      .append("text")
       .attr("font-size", "10px")
       .attr("text-anchor", "left")
       .attr("transform", `translate(${15},${8})`)
       .text("Not Depressed");
 
+    // plot 3: Sankey
+    const color = "#77ACA2";
+    const sankeyHighlightColor = "orange";
+    const sankeySvg = d3.selectAll("#sankey-svg");
+
+    sankeySvg
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height)
+      .style("fill", "#F4E9CD");
+
+    const sankey = d3
+      .sankey()
+      .nodeSort(null)
+      .linkSort(null)
+      .nodeWidth(4)
+      .nodePadding(1)
+      .extent([
+        [sankeyMargin.left, sankeyMargin.top],
+        [width - sankeyMargin.right, height - sankeyMargin.bottom],
+      ]);
+
+    const graphData = graph(processedData);
+    console.log("graphdata", graphData)
+    const { nodes, links } = sankey({
+      nodes: graphData.nodes.map((d) => Object.create(d)),
+      links: graphData.links.map((d) => Object.create(d)),
+    });
+
+    // Vertical lines
+    sankeySvg
+      .append("g")
+      .selectAll("rect")
+      .data(nodes)
+      .join("rect")
+      .attr("x", (d) => d.x0)
+      .attr("y", (d) => d.y0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("width", (d) => d.x1 - d.x0)
+      .append("title")
+      .text((d) => `${d.name}\n`);
+
+    // Links
+    const lines = sankeySvg
+      .append("g")
+      .attr("fill", "none")
+      .selectAll("g")
+      .data(links)
+      .join("path")
+      .attr("d", d3.sankeyLinkHorizontal())
+      .attr("stroke", (d) => color)
+      .attr("stroke-width", (d) => d.width)
+      .style("mix-blend-mode", "multiply")
+      .append("title")
+      .text((d) => `${d.names.join(" → ")}\n`);
+
+    // Node labels
+    sankeySvg
+      .append("g")
+      .style("font", "10px sans-serif")
+      .selectAll("text")
+      .data(nodes)
+      .join("text")
+      .attr("x", (d) => (d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 3))
+      .attr("y", (d) => (d.y1 + d.y0) / 2)
+      .attr("dx", (d) => {
+        if (d.x0 < width / 2 - 50) {
+          return "-2em";
+        } else if (d.x0 > width / 2 + 50) {
+          return "1.4em";
+        } else {
+          return "1.0em";
+        }
+      })
+      .attr("dy", "0.35em")
+      .attr("text-anchor", (d) => (d.x0 < width / 2 ? "end" : "start"))
+      .attr("font-size", (d) => {
+        if (d.x0 < width / 2 - 50) {
+          return 7;
+        } else if (d.x0 > width / 2 + 50) {
+          return 6;
+        } else {
+          return 9;
+        }
+      })
+      .text((d) => d.name)
+      .append("tspan")
+      .attr("fill-opacity", 0.7);
   })
   .catch(function (error) {
     console.log(error);
@@ -256,4 +356,57 @@ function drawPie(svg, data, pieWidth, pieHeight, radius, x, y, text) {
     .attr("font-size", "10px")
     .attr("text-anchor", "middle")
     .text(text);
+}
+
+// Modified from tutorial: https://observablehq.com/@d3/parallel-sets
+function graph(processedData) {
+  const keys = ["Gender", "Year", "GPA"];
+  let index = -1;
+  const nodes = [];
+  const nodeByKey = new d3.InternMap([], JSON.stringify);
+  const indexByKey = new d3.InternMap([], JSON.stringify);
+  const links = [];
+
+  for (const k of keys) {
+    for (const d of processedData) {
+      const key = [k.toLowerCase(), d[k.toLowerCase()]];
+      if (nodeByKey.has(key)) continue;
+      const node = { name: d[k.toLowerCase()] };
+      nodes.push(node);
+      nodeByKey.set(key, node);
+      indexByKey.set(key, ++index);
+    }
+  }
+
+  console.log("nodeByKey", nodeByKey)
+  console.log("indexByKey", indexByKey)
+
+  for (let i = 1; i < keys.length; ++i) {
+    const a = keys[i - 1].toLowerCase();
+    const b = keys[i].toLowerCase();
+    const prefix = keys.slice(0, i + 1);
+    // console.log(prefix)
+    const linkByKey = new d3.InternMap([], JSON.stringify);
+    for (const d of processedData) {
+      const names = prefix.map((k) => d[k.toLowerCase()]);
+      console.log(names)
+      const value = d.value || 1;
+      let link = linkByKey.get(names);
+      if (link) {
+        link.value += value;
+        continue;
+      }
+      link = {
+        source: indexByKey.get([a, d[a.toLowerCase()]]),
+        target: indexByKey.get([b, d[b.toLowerCase()]]),
+        names,
+        value,
+        gameName: d.Name,
+      };
+      links.push(link);
+      linkByKey.set(names, link);
+    }
+  }
+
+  return { nodes, links };
 }
