2e464e85ec91b987ff6a9017a3927afe194fb27e
Naomi Boegholm <naomiboegholm@Naomis-Noggin.local>
2026-05-14 21:22:24 -0700

    github test

---
+++ Homework2/nlboegholm/main.js
@@ -0,0 +1,8 @@
+    const svg = d3.select("svg");
+    const g1 = svg.append("g");
+    
+    g1.append("text")
+    .attr("x", 100)
+    .attr("y", 100)
+    .text("hello wooorrrrllldddd hello hello hello hello hello hello hello hello hello");
+
