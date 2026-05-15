271a0ee70e697f03136ce88b77fb9facbee7c6e9
Xinyi Li <ccwli@ucdavis.edu>
2026-05-14 18:38:48 -0700

    finish plots, remain interact

--- Homework2/ccwli/main.js
+++ Homework2/ccwli/main.js
@@ -2,45 +2,60 @@ let abFilter = 25;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-let scatterLeft = 0, scatterTop = 0;
-let scatterMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    scatterWidth = 400 - scatterMargin.left - scatterMargin.right,
-    scatterHeight = 350 - scatterMargin.top - scatterMargin.bottom;
+const svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-let distrLeft = 400, distrTop = 0;
-let distrMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    distrWidth = 400 - distrMargin.left - distrMargin.right,
-    distrHeight = 350 - distrMargin.top - distrMargin.bottom;
+// layout
+let scatterMargin = {top: 50, right: 30, bottom: 60, left: 60};
+let scatterWidth = 250;
+let scatterHeight = 250; //sqaure
+let scatterLeft = 80;
 
-let teamLeft = 0, teamTop = 400;
-let teamMargin = {top: 10, right: 30, bottom: 30, left: 60},
-    teamWidth = width - teamMargin.left - teamMargin.right,
-    teamHeight = height-450 - teamMargin.top - teamMargin.bottom;
+let distrMargin = {top: 50, right: 30, bottom: 60, left: 60};
+let distrTop = 50; 
+let distrWidth = 250; 
+let distrHeight = 250;
+let distrLeft = scatterLeft + scatterWidth + 150;
+
+let teamMargin = {top: 50, right: 20, bottom: 50, left: 80};
+let teamTop = 420; 
+let teamWidth = width - teamMargin.left - teamMargin.right -80;
+let teamHeight = 250;
 
 // plots
 d3.csv("student-mat.csv").then(rawData =>{
     console.log("rawData", rawData);
+    const tooltip = d3.select("#tooltip");
 
-    rawData.forEach(function(d){
+    rawData.forEach(function(d,i){
+        d.id = i;
         d.studytime = Number(d.studytime);
         d.G3 = Number(d.G3);
         d.absences = Number(d.absences);
+        d.Walc = Number(d.Walc);
+        d.Dalc = Number(d.Dalc);
     });
 
 
     const filteredData = rawData
     const processedData = filteredData.map(d=>{
                           return {
-                              'studytime': d.studytime,
-                              'G3': d.G3,
-                              'absences': d.absences,
-                              'sex': d.sex
+                            'id': d.id,
+                            'studytime': d.studytime,
+                            'G3': d.G3,
+                            'absences': d.absences,
+                            'sex': d.sex,
+                            'Walc':d.Walc,
+                            'Dalc': d.Dalc
                           };
     });
     console.log("processedData", processedData);
-
-    //plot 1: Scatter Plot
+ 
+    //plot 1: Scatter Plot for study time and final grade
+  
     const svg = d3.select("svg");
+    
 
     const g1 = svg.append("g")
                 .attr("width", scatterWidth + scatterMargin.left + scatterMargin.right)
@@ -51,7 +66,7 @@ d3.csv("student-mat.csv").then(rawData =>{
     g1.append("text")
     .attr("x", scatterWidth / 2)
     .attr("y", scatterHeight + 50)
-    .attr("font-size", "20px")
+    .attr("font-size", "16px")
     .attr("text-anchor", "middle")
     .text("Study Time");
 
@@ -60,7 +75,7 @@ d3.csv("student-mat.csv").then(rawData =>{
     g1.append("text")
     .attr("x", -(scatterHeight / 2))
     .attr("y", -40)
-    .attr("font-size", "20px")
+    .attr("font-size", "16px")
     .attr("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
     .text("Final Grade (G3)");
@@ -90,6 +105,34 @@ d3.csv("student-mat.csv").then(rawData =>{
                         .ticks(13);
     g1.append("g").call(yAxisCall);
 
+     //legend
+
+    const legend = svg.append("g")
+    .attr("transform", "translate(20,20)");
+
+    legend.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "#ff69b4");
+
+    legend.append("text")
+        .attr("x", 15)
+        .attr("y", 10)
+        .text("Female");
+
+    legend.append("rect")
+        .attr("x", 80)
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "#4682b4");
+
+    legend.append("text")
+        .attr("x", 95)
+        .attr("y", 10)
+        .text("Male");
+
+
+
     // circles
     const circles = g1.selectAll("circle").data(processedData);
 
@@ -97,81 +140,304 @@ d3.csv("student-mat.csv").then(rawData =>{
          .attr("cx", d => x1(d.studytime))
          .attr("cy", d => y1(d.G3))
          .attr("r", 5)
-         .attr("fill", d=> d.sex==='F'? '#ff69b4':'#4682b4');
+         .attr("class", d => "scatter-dot dot-" + d.id)
+         .attr("fill", d=> d.sex==='F'? '#ff69b4':'#4682b4')
+         .on("mouseover", handleMouseOver)
+         .on("mouseout", handleMouseOut);
 
     const g2 = svg.append("g")
                 .attr("width", distrWidth + distrMargin.left + distrMargin.right)
                 .attr("height", distrHeight + distrMargin.top + distrMargin.bottom)
                 .attr("transform", `translate(${distrLeft}, ${distrTop})`);
 
-    //plot 2: Bar Chart for Team Player Count
+    //plot 2: Heatmap for alcohol consumption and final grade
 
-    const teamCounts = processedData.reduce((s, { teamID }) => (s[teamID] = (s[teamID] || 0) + 1, s), {});
-    const teamData = Object.keys(teamCounts).map((key) => ({ teamID: key, count: teamCounts[key] }));
-    console.log("teamData", teamData);
+    console.log('processedData', processedData);
+
+    const heatmapData=[];
+    for (let walc =1; walc<=5; walc++){
+        for (let dalc=1; dalc<=5; dalc++){
+            const group = processedData.filter(d=>
+                d.Walc === walc && d.Dalc === dalc
+            );
+
+            const avgG3 = d3.mean(group, d=> d.G3);
+
+            heatmapData.push({
+                Walc: walc,
+                Dalc: dalc,
+                avgG3: avgG3||0
+            });
+        }
+    }
+    console.log("heatmapData", heatmapData)
+    
 
 
-    const g3 = svg.append("g")
-                .attr("width", teamWidth + teamMargin.left + teamMargin.right)
-                .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
-                .attr("transform", `translate(${teamMargin.left}, ${teamTop})`);
+    // x scale
+    const x2 = d3.scaleBand()
+        .domain([1,2,3,4,5])
+        .range([0,distrWidth]);
 
-    // X label
-    g3.append("text")
-    .attr("x", teamWidth / 2)
-    .attr("y", teamHeight + 50)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("Team");
+    // Y scale
+    const y2= d3.scaleBand()
+        .domain([1,2,3,4,5])
+        .range([distrHeight, 0]);
+
+    //color
+
+    const colorScale = d3.scaleSequential()
+        .interpolator(d3.interpolateYlOrRd)
+        .domain([0, 20]);
+
+    // x axis
+    g2.append('g')
+        .attr('transform', `translate(0, ${distrHeight})`)
+        .call(d3.axisBottom(x2));
+
+    //y axis
+    g2.append('g')
+        .call(d3.axisLeft(y2));
+
+    // Heatmap squares
+    g2.selectAll("rect")
+        .data(heatmapData)
+        .enter()
+        .append("rect")
+        .attr("x", d => x2(d.Walc))
+        .attr("y", d => y2(d.Dalc))
+        .attr("width", x2.bandwidth())
+        .attr("height", y2.bandwidth())
+        .attr("fill", d => colorScale(d.avgG3))
+
+        .on("mouseover", function(event, d) {
+            tooltip
+                .style("opacity", 1)
+                .style("pointer-events", "all")
+                .html(
+                    `Walc: ${d.Walc}<br>
+                    Dalc: ${d.Dalc}<br>
+                    Avg G3: ${d.avgG3.toFixed(2)}`
+                );
+        })
+
+        .on("mousemove", function(event) {
+            tooltip
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY + 10) + "px");
+        })
+
+        .on("mouseout", function() {
+            tooltip.style("opacity", 0);
+        });
+
+
+    // Title
+    g2.append("text")
+        .attr("x", distrWidth / 2)
+        .attr("y", -15)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .text("Alcohol Consumption vs Final Grade (G3)");
+
+
+     // X label
+    g2.append("text")
+        .attr("x", distrWidth/2)
+        .attr("y", distrHeight + 50)
+        .attr("font-size", "16px")
+        .attr("text-anchor", "middle")
+        .text("Weekend Alcohol Consumption (Walc)");
 
 
     // Y label
+    g2.append("text")
+        .attr("x", -(distrHeight/2))
+        .attr("y", -40)
+        .attr("font-size", "16px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("Workday Alcohol Consumption (Dalc)");
+
+    // eatmap Legend
+    const legendHeight = 150;
+    const legendWidth = 15;
+
+    const legendScale = d3.scaleLinear()
+        .domain([0, 20])
+        .range([legendHeight, 0]);
+
+    const legendAxis = d3.axisRight(legendScale)
+        .ticks(5);
+
+    const defs = svg.append("defs");
+
+    const linearGradient = defs.append("linearGradient")
+        .attr("id", "heatmap-gradient")
+        .attr("x1", "0%")
+        .attr("y1", "100%")
+        .attr("x2", "0%")
+        .attr("y2", "0%");
+
+    // color stops
+    linearGradient.selectAll("stop")
+        .data([
+            {offset: "0%", color: d3.interpolateYlOrRd(0)},
+            {offset: "25%", color: d3.interpolateYlOrRd(0.25)},
+            {offset: "50%", color: d3.interpolateYlOrRd(0.5)},
+            {offset: "75%", color: d3.interpolateYlOrRd(0.75)},
+            {offset: "100%", color: d3.interpolateYlOrRd(1)}
+        ])
+        .enter()
+        .append("stop")
+        .attr("offset", d => d.offset)
+        .attr("stop-color", d => d.color);
+
+    const hmlegend = g2.append("g")
+        .attr("transform", `translate(${distrWidth + 30}, 20)`);
+
+    // gradient rect
+    hmlegend.append("rect")
+        .attr("width", legendWidth)
+        .attr("height", legendHeight)
+        .style("fill", "url(#heatmap-gradient)");
+
+    // axis
+    hmlegend.append("g")
+        .attr("transform", `translate(${legendWidth}, 0)`)
+        .call(legendAxis);
+
+    // label
+    hmlegend.append("text")
+        .attr("x", -10)
+        .attr("y", -10)
+        .text("Avg G3")
+        .style("font-size", "12px");
+
+
+    //Plot 3: Parallel Coordinates Plot
+
+
+    const dimensions = ['studytime', 'absences', 'Dalc', 'Walc', 'G3'];
+
+    // y scale
+    const yScales = {};
+    dimensions.forEach(name => {
+        yScales[name] = d3.scaleLinear()
+            .domain(d3.extent(processedData, d => +d[name]))
+            .range([teamHeight, 0]);
+    });
+
+    // x scale
+    const xScalePCP = d3.scalePoint()
+        .range([0, teamWidth])
+        .domain(dimensions);
+
+    // lineup
+    function path(d) {
+        return d3.line()(dimensions.map(p => [xScalePCP(p), yScales[p](d[p])]));
+    }
+
+    const g3 = svg.append("g")
+        .attr("width", teamWidth + teamMargin.left + teamMargin.right)
+        .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
+        .attr("transform", `translate(${teamMargin.left}, ${teamTop})`);
+
+    // plot line
+    g3.selectAll("myPath")
+        .data(processedData)
+        .enter().append("path")
+        .attr("class", d => "line line-" + d.id)
+        .attr("d", path)
+        .style("fill", "none")
+        .style("stroke", d => d.sex === 'F' ? '#ff69b4' : '#4682b4')
+        .style("opacity", 0.4);
+
+    // y axis
+    g3.selectAll("myAxis")
+        .data(dimensions).enter()
+        .append("g")
+        .attr("transform", d => `translate(${xScalePCP(d)})`)
+        .each(function(d) { d3.select(this).call(d3.axisLeft(yScales[d])); })
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -10)
+        .text(d => d)
+        .style("fill", "black")
+        .style("font-size", "12px");
+
+    // title
     g3.append("text")
-    .attr("x", -(teamHeight / 2))
-    .attr("y", -40)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("Number of players");
+        .attr("x", teamWidth / 2)
+        .attr("y", -25)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .text("Multivariate Analysis: Factors impacting G3");
 
-    // X ticks
-    const x2 = d3.scaleBand()
-    .domain(teamData.map(d => d.teamID))
-    .range([0, teamWidth])
-    .paddingInner(0.3)
-    .paddingOuter(0.2);
+    // legend
 
-    const xAxisCall2 = d3.axisBottom(x2);
-    g3.append("g")
-    .attr("transform", `translate(0, ${teamHeight})`)
-    .call(xAxisCall2)
-    .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
+    const pcpLegend = g3.append("g")
+        .attr("transform", `translate(${teamWidth -90}, -35)`); // 放在右上角，标题附近
 
-    // Y ticks
-    const y2 = d3.scaleLinear()
-    .domain([0, d3.max(teamData, d => d.count)])
-    .range([teamHeight, 0])
-    .nice();
+    // female
+    pcpLegend.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "#ff69b4");
 
-    const yAxisCall2 = d3.axisLeft(y2)
-                        .ticks(6);
-    g3.append("g").call(yAxisCall2);
+    pcpLegend.append("text")
+        .attr("x", 15)
+        .attr("y", 10)
+        .style("font-size", "12px")
+        .text("Female");
 
-    // bars
-    const bars = g3.selectAll("rect").data(teamData);
+    // male
+    pcpLegend.append("rect")
+        .attr("y", 20) 
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", "#4682b4");
 
-    bars.enter().append("rect")
-    .attr("y", d => y2(d.count))
-    .attr("x", d => x2(d.teamID))
-    .attr("width", x2.bandwidth())
-    .attr("height", d => teamHeight - y2(d.count))
-    .attr("fill", "steelblue");
+    pcpLegend.append("text")
+        .attr("x", 15)
+        .attr("y", 30)
+        .style("font-size", "12px")
+        .text("Male");
 
 
-    }).catch(function(error){
+// interaction
+function handleMouseOver(event,d) {
+    // other dots and path darken
+    d3.selectAll(".scatter-dot").style("opacity", 0.1);
+    d3.selectAll(".line").style("opacity", 0.05);
+
+    // highlight selected dot
+    d3.select(this)
+        .style("opacity", 1)
+        .attr("r", 8)
+        .style("stroke", "black");
+
+    // highlight corresponding path
+    d3.selectAll(".line-" + d.id)
+        .style("opacity", 1)
+        .style("stroke-width", "4px")
+        .style("stroke", "orange");
+}
+
+// back to normal
+function handleMouseOut(event,d) {
+    d3.selectAll(".scatter-dot")
+        .style("opacity", 1)
+        .attr("r", 5)
+        .style("stroke", "none");
+        
+    d3.selectAll("path")
+        .style("opacity", 0.4)
+        .style("stroke-width", "1.5px")
+        .style("stroke", d => d.sex === 'F' ? '#ff69b4' : '#4682b4');
+}
+
+
+}).catch(function(error){
     console.log(error);
 });
