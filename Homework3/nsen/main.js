a0a77d40f471d8cce99386d2eaf94c80d6b1b86e
nithinsenthil <nithin020304@gmail.com>
2026-05-18 14:44:38 -0700

    fix: chartDims invalid declaration bug

--- Homework3/nsen/main.js
+++ Homework3/nsen/main.js
@@ -19,21 +19,21 @@ const chartDims = {
   bar: {
     width: width / 2,
     height: height / 2 - headerHeight / 2,
-    innerWidth: chartDims.bar.width - margins.left - margins.right,
-    innerHeight: chartDims.bar.height - margins.top - margins.bottom,
+    innerWidth: width / 2 - margins.left - margins.right,
+    innerHeight: height / 2 - headerHeight / 2 - margins.top - margins.bottom,
   },
   scatter: {
     width: width / 2,
     height: height / 2 - headerHeight / 2,
-    innerWidth: chartDims.scatter.width - margins.left - margins.right,
-    innerHeight: chartDims.scatter.height - margins.top - margins.bottom,
+    innerWidth: width / 2 - margins.left - margins.right,
+    innerHeight: height / 2 - headerHeight / 2 - margins.top - margins.bottom,
   },
   parallel: {
     width: width / 2,
     height: height - headerHeight,
-    innerWidth: chartDims.parallel.width - margins.left - margins.right,
+    innerWidth: width / 2 - margins.left - margins.right,
     innerHeight:
-      chartDims.parallel.height - margins.top - margins.bottom - keyHeight,
+      height - headerHeight - margins.top - margins.bottom - keyHeight,
   },
 };
 
